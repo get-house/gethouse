297b5d41110438de305be7c14f222b1dd99ed1ef
Hussain Aminu <hussain_aminu@yahoo.com>
2022-07-24 20:38:25 +0100

    added laravel telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->hourly();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
@@ -34,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
